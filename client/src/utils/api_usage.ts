5a3ec51967934d74555e520c446a2004bdfb6297
= <uppunuthula925@gmail.com>
2023-02-13 12:06:25 +0530

    Added prod API

--- client/src/utils/api_usage.ts
+++ client/src/utils/api_usage.ts
@@ -1,6 +1,7 @@
 export let API: string;
 if (import.meta.env.VITE_NODE_ENV === "production") {
   API = import.meta.env.VITE_PRODUCTION_API;
+  console.log(API);
 } else {
   API = import.meta.env.VITE_DEVELOPMENT_API;
 }
